94abd1c2edf466b61ef2cd90457af4e9452d3721
alexgrin1 <a_grynevich@mail.ru>
2023-05-31 00:00:12 +0300

    Create imports file

--- lib/imports.dart
+++ lib/imports.dart
@@ -1,2 +1,21 @@
 export 'package:flutter/material.dart';
 export 'package:news_app/common/theme.dart';
+
+// packages
+
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:news_app/logic/page_controll_provider.dart';
+export 'package:provider/provider.dart';
+export 'package:extended_image/extended_image.dart';
+
+//screens
+export 'package:news_app/view/news/news.dart';
+export 'package:news_app/view/home/widgets/bottom_nav_bar.dart';
+export 'package:news_app/view/home/home.dart';
+
+export 'package:news_app/view/news/widgets/everything.dart';
+export 'package:news_app/view/news/widgets/top_headlines.dart';
+
+//widgets
+export 'package:news_app/view/favorites/favorites.dart';
+export 'package:news_app/view/news/widgets/news_card.dart';
