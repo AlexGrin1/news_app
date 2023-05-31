ade2958d7b361424124ec1c85fec8850a6188495
alexgrin1 <a_grynevich@mail.ru>
2023-05-31 18:50:21 +0300

    create http service and local data service

---
+++ lib/repository/api/http_service.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class ApiResponse<T> {
+  int statusCode;
+  Map<String, dynamic> body;
+  ApiResponse({required this.statusCode, required this.body});
+
+  factory ApiResponse.fromJson(int statusCode, Map<String, dynamic> json) {
+    return ApiResponse<T>(statusCode: statusCode, body: json);
+  }
+}
+
+class NewsApi {
+  final String baseUrl = 'https://newsapi.org/v2';
+  final String _api_key =
+      '0fb0db55b74e4efa81ce182d12de2a6e'; //'38823f356abb4c6d9fa19f68dd78b40b';
+  final String country = 'us';
+  final String questionRequest = 'apple';
+  final int pageSize = 15;
+
+  getRequest({required String url, Map<String, String>? headers}) async {
+    int? statusCode;
+    try {
+      var response = await http.get(Uri.parse(url));
+
+      statusCode = response.statusCode;
+
+      return ApiResponse(
+          statusCode: statusCode, body: json.decode(response.body));
+    } catch (e) {
+      return ApiResponse.fromJson(statusCode ?? 0, {"error": "$e"});
+    }
+  }
+
+  Future<ApiResponse> getEverythingNews({required int page}) async {
+    String url =
+        "$baseUrl/everything?q=$questionRequest&page=$page&pageSize=$pageSize&apiKey=$_api_key";
+
+    ApiResponse response = await getRequest(url: url);
+    return ApiResponse.fromJson(response.statusCode, response.body);
+  }
+
+  Future<ApiResponse> getTopHeadlinesNews({required int page}) async {
+    String url = "$baseUrl/top-headlines?country=$country&apiKey=$_api_key";
+
+    ApiResponse response = await getRequest(url: url);
+    return ApiResponse.fromJson(response.statusCode, response.body);
+  }
+}
---
+++ lib/repository/data/local_data.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import "package:flutter/foundation.dart";
+import 'package:news_app/repository/models/article.dart';
+import "package:path_provider/path_provider.dart";
+import 'package:hive/hive.dart';
+
+class LocalData {
+  late Box<dynamic> storage;
+  initializeLocalData() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
+    storage = await Hive.openBox("storage");
+  }
+
+  Future<List<Article>> getLocalData() async {
+    var data = await storage.values;
+    log('data $data');
+    return List.from(data)
+        .map((e) => Article.fromJson(json.decode(e)))
+        .toList();
+  }
+
+  isFavourites({required String key}) {
+    return storage.get(key) != null && storage.get(key).isNotEmpty
+        ? true
+        : false;
+  }
+
+  saveToStorage({required String key, required String value}) {
+    storage.put(key, value);
+  }
+
+  removeFromStorage({required String key}) {
+    storage.delete(key);
+  }
+}
