7b44a4c61b0ae436ff5441577c4af0827954c032
alexgrin1 <a_grynevich@mail.ru>
2023-05-31 18:49:20 +0300

    update theme and config

---
+++ lib/common/config.dart
@@ -0,0 +1,7 @@
+import 'package:news_app/imports.dart';
+import 'package:news_app/repository/data/local_data.dart';
+
+final NewsAppTheme newsAppTheme = NewsAppTheme();
+final Controller controller = Controller();
+final NewsApi newsApi = NewsApi();
+final LocalData localData = LocalData();
--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,35 +1,42 @@
 import 'package:news_app/imports.dart';
 
-import 'package:flutter/material.dart';
-
-NewsAppTheme newsAppTheme = NewsAppTheme();
-
 class NewsAppTheme {
-  AppTypography typography = AppTypography();
-  AppColors appColors = AppColors();
+  final AppTypography typography = AppTypography();
+  final AppColors appColors = AppColors();
 }
 
 class AppColors {
   Color highlighting = const Color.fromARGB(255, 0, 202, 192);
   Color light = const Color(0xffffffff);
+  Color lightWithOpacity05 = const Color(0xffffffff).withOpacity(0.5);
   Color dark = const Color.fromARGB(255, 48, 48, 48);
   Color background = const Color.fromARGB(255, 65, 65, 65);
 }
 
 class AppTypography {
   TextStyle regular24 = const TextStyle(
-    color: Colors.white,
+    color: Color(0xffffffff),
     fontFamily: "Roboto-Regular",
     fontSize: 24.0,
   );
   TextStyle regular14 = const TextStyle(
-    color: Colors.white,
+    color: Color(0xffffffff),
     fontFamily: "Roboto-Regular",
     fontSize: 14,
   );
   TextStyle semibold16 = const TextStyle(
-    color: Colors.white,
+    color: Color(0xffffffff),
     fontFamily: "Roboto-Bold",
     fontSize: 16,
+  );
+  TextStyle newsTitle = const TextStyle(
+    color: Color(0xffffffff),
+    fontFamily: "Roboto-Medium",
+    fontSize: 16.0,
+  );
+  TextStyle newsDesciption = TextStyle(
+    color: Color(0xffffffff).withOpacity(0.5),
+    fontFamily: "Roboto-Regular",
+    fontSize: 14.0,
   );
 }
