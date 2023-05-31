772ea4270160a4a9616ba22984adeb9c16be83e6
alexgrin1 <a_grynevich@mail.ru>
2023-05-31 18:52:35 +0300

    update imports

--- lib/imports.dart
+++ lib/imports.dart
@@ -1,21 +1,35 @@
-export 'package:flutter/material.dart';
 export 'package:news_app/common/theme.dart';
-
+export 'package:news_app/common/config.dart';
 // packages
-
+export 'package:flutter/material.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
-export 'package:news_app/logic/page_controll_provider.dart';
+export 'package:equatable/equatable.dart';
 export 'package:provider/provider.dart';
 export 'package:extended_image/extended_image.dart';
-
+export 'package:shimmer/shimmer.dart';
+export 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 //screens
-export 'package:news_app/view/news/news.dart';
-export 'package:news_app/view/home/widgets/bottom_nav_bar.dart';
-export 'package:news_app/view/home/home.dart';
+export 'package:news_app/presentation/news/news.dart';
+export 'package:news_app/presentation/home/widgets/bottom_nav_bar.dart';
+export 'package:news_app/presentation/home/home.dart';
 
-export 'package:news_app/view/news/widgets/everything.dart';
-export 'package:news_app/view/news/widgets/top_headlines.dart';
+export 'package:news_app/presentation/news/widgets/everything.dart';
+export 'package:news_app/presentation/news/widgets/top_headlines.dart';
 
 //widgets
-export 'package:news_app/view/favorites/favorites.dart';
-export 'package:news_app/view/news/widgets/news_card.dart';
+export 'package:news_app/presentation/favorites/favorites.dart';
+export 'package:news_app/presentation/news/widgets/news_card.dart';
+export 'package:news_app/presentation/widgets/loading.dart';
+
+// models
+export 'package:news_app/repository/models/news.dart';
+export 'package:news_app/repository/models/article.dart';
+export 'package:news_app/repository/models/source.dart';
+
+// services
+
+export 'package:news_app/repository/api/http_service.dart';
+export 'package:news_app/repository/controllers/controllers.dart';
+
+//logic
+export 'package:news_app/logic/providers/page_controll_provider.dart';
